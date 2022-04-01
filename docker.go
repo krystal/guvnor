0771bf2f9edb8ad928038c2730f3f766114d0cc0
root <noah@noahstride.co.uk>
2022-04-01 11:31:58 +0000

    fix: correctly load auth config from docker config

--- docker.go
+++ docker.go
@@ -5,9 +5,11 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"os"
 	"path"
+	"strings"
 
 	"github.com/docker/distribution/reference"
 	"github.com/docker/docker/api/types"
@@ -33,7 +35,50 @@ func getIndexForImage(image string) (string, error) {
 	return reg.Index.Name, nil
 }
 
-func loadCredentialsFromDockerConfig(image string) (string, error) {
+func extractAuthCfg(configBytes []byte, index string) (*types.AuthConfig, error) {
+	// Useful excerpts from dockers source code:
+	// https://github.com/docker/cli/blob/ae3b0b34c838ff37ead3b43a6081d33656a57c07/cli/config/configfile/file.go#L119
+	// https://github.com/docker/cli/blob/ae3b0b34c838ff37ead3b43a6081d33656a57c07/cli/config/configfile/file.go#L274
+
+	dockerCfg := &dockerAuthConfig{}
+	if err := json.Unmarshal(configBytes, dockerCfg); err != nil {
+		return nil, err
+	}
+
+	authCfg, ok := dockerCfg.Auths[index]
+	if !ok {
+		return nil, errors.New("no auth configured for registry")
+	}
+
+	authCfg.ServerAddress = index
+
+	// If "auth" field is provided, then we can extract username and password from this field
+	if authCfg.Auth != "" {
+		authBytes, err := base64.StdEncoding.DecodeString(authCfg.Auth)
+		if err != nil {
+			return nil, err
+		}
+
+		arr := strings.Split(string(authBytes), ":")
+		if len(arr) != 2 {
+			return nil, fmt.Errorf("auth string malformed, expected 2 parts")
+		}
+
+		authCfg.Username = arr[0]
+		authCfg.Password = arr[1]
+		authCfg.Auth = ""
+	}
+
+	if authCfg.Username == "" {
+		return nil, fmt.Errorf(
+			"no auth options provided for registry",
+		)
+	}
+
+	return &authCfg, nil
+}
+
+func authStringForImage(image string) (string, error) {
 	home, err := os.UserHomeDir()
 	if err != nil {
 		return "", err
@@ -45,22 +90,17 @@ func loadCredentialsFromDockerConfig(image string) (string, error) {
 		return "", err
 	}
 
-	dockerConf := &dockerAuthConfig{}
-	if err := json.Unmarshal(data, dockerConf); err != nil {
-		return "", err
-	}
-
 	indexName, err := getIndexForImage(image)
 	if err != nil {
 		return "", err
 	}
 
-	registryAuth, ok := dockerConf.Auths[indexName]
-	if !ok || (registryAuth.Auth == "" && registryAuth.Username == "") {
-		return "", errors.New("no auth configured")
+	authConfig, err := extractAuthCfg(data, indexName)
+	if err != nil {
+		return "", err
 	}
 
-	outBytes, err := json.Marshal(registryAuth)
+	outBytes, err := json.Marshal(authConfig)
 	if err != nil {
 		return "", err
 	}
@@ -71,7 +111,7 @@ func loadCredentialsFromDockerConfig(image string) (string, error) {
 // pullImage will ensure that an image exists in the local store. This means
 // it will not pull if it is already present.
 func (e *Engine) pullImage(ctx context.Context, image string) error {
-	authStr, err := loadCredentialsFromDockerConfig(image)
+	authStr, err := authStringForImage(image)
 	if err != nil {
 		e.log.Info(
 			"could not load docker credentials, using no auth",
--- docker_test.go
+++ docker_test.go
@@ -3,6 +3,7 @@ package guvnor
 import (
 	"testing"
 
+	"github.com/docker/docker/api/types"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -28,6 +29,61 @@ func Test_getIndexforImage(t *testing.T) {
 		t.Run(t.Name(), func(t *testing.T) {
 			got, err := getIndexForImage(tt.image)
 			assert.NoError(t, err)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_extractAuthCfg(t *testing.T) {
+	tests := []struct {
+		name      string
+		dockerCfg []byte
+		want      *types.AuthConfig
+		wantErr   string
+	}{
+		{
+			name:      "blank docker config",
+			dockerCfg: []byte("{}"),
+			wantErr:   "no auth configured for registry",
+		},
+		{
+			name:      "no auth configured for registry",
+			wantErr:   "no auth configured for registry",
+			dockerCfg: []byte(`{"auths":{}}`),
+		},
+		{
+			name:      "user/password provided",
+			dockerCfg: []byte(`{"auths":{"test.com":{"username":"user","password":"pass"}}}`),
+			want: &types.AuthConfig{
+				ServerAddress: "test.com",
+				Username:      "user",
+				Password:      "pass",
+			},
+		},
+		{
+			name:      "auth provided",
+			dockerCfg: []byte(`{"auths":{"test.com":{"auth":"dXNlcjpwYXNz"}}}`),
+			want: &types.AuthConfig{
+				ServerAddress: "test.com",
+				Username:      "user",
+				Password:      "pass",
+			},
+		},
+		{
+			name:      "malformed auth provided",
+			dockerCfg: []byte(`{"auths":{"test.com":{"auth":"dXNlcjpwYXNzOmhlaA=="}}}`),
+			wantErr:   "auth string malformed, expected 2 parts",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := extractAuthCfg(tt.dockerCfg, "test.com")
+			if tt.wantErr != "" {
+				assert.EqualError(t, err, tt.wantErr)
+			} else {
+				assert.NoError(t, err)
+			}
 			assert.Equal(t, tt.want, got)
 		})
 	}
